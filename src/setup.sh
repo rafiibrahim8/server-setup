c470f5a5e407fe9ec2ba38503005a84889158dda
Ibrahim Rafi <rafiibrahim8@hotmail.com>
2024-01-09 02:16:28 +0600

    Update setup.sh

--- src/setup.sh
+++ src/setup.sh
@@ -35,7 +35,7 @@ setup_nodejs() {
     sudo mkdir -p /etc/apt/keyrings
     curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
     NODE_MAJOR=20
-    echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
     apt update
     apt install -y nodejs
     
