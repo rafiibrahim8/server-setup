3e86af700a6d9caa3e0cd8edeec376f0976e4c0b
Ibrahim Rafi <rafiibrahim8@hotmail.com>
2024-07-31 00:53:47 +0600

    chmod

--- src/hpu.sh
+++ src/hpu.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 echo "Running HPU script as user: $USER"
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/authorized_keys
--- src/lpu.sh
+++ src/lpu.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 echo "Running LPU script as user: $USER"
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/authorized_keys
