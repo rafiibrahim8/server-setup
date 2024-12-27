744ebde0c6dcc4ff8defea70deca2bee323de37d
Ibrahim Rafi <rafiibrahim8@hotmail.com>
2024-12-27 15:46:17 +0600

    Update config.fish

--- fs/etc/skel/.config/fish/config.fish
+++ fs/etc/skel/.config/fish/config.fish
@@ -47,10 +47,10 @@ end
 
 
 # Replace ls with exa
-alias ls='exa -al --color=always --group-directories-first --icons' # preferred listing
-alias la='exa -a --color=always --group-directories-first --icons'  # all files and dirs
-alias ll='exa -l --color=always --group-directories-first --icons'  # long format
-alias lt='exa -aT --color=always --group-directories-first --icons' # tree listing
+alias ls='exa -al --color --group-directories-first --icons' # preferred listing
+alias la='exa -a --color --group-directories-first --icons'  # all files and dirs
+alias ll='exa -l --color --group-directories-first --icons'  # long format
+alias lt='exa -aT --color --group-directories-first --icons' # tree listing
 alias l.="exa -a | egrep '^\.'"                                     # show only dotfiles
 alias ip="ip -color"
 
